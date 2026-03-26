812bb289cdd24d99fe1bda34f06a29298f49e992
Huseyn Gaflanov <huseyng@proton.me>
2026-03-27 01:15:45 +0400

    Photo preview fixes.

--- functions/api/photos.js
+++ functions/api/photos.js
@@ -1,5 +1,7 @@
 export async function onRequest(context) {
   const objects = await context.env.PHOTOS.list({ prefix: 'originals/' });
-  const keys = objects.objects.map(obj => obj.key.replace('originals/', ''));
+  const keys = objects.objects
+    .map(obj => obj.key.replace('originals/', ''))
+    .filter(key => key !== '');
   return Response.json(keys);
 }
